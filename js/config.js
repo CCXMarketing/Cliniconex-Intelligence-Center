18707c99ad832a6b6f267608aeef08e5db8bee28
CCXMarketing <gerald@cliniconex.com>
2026-04-07 12:49:55 -0400

    Add configuration file for API keys and settings

---
+++ js/config.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+  activecampaign: {
+    api_url: 'https://cliniconexmarketing.api-us1.com/api/3',
+    api_key: '5d8ab566d14db85263d23a58eb269d1381e03cf0658ff95f1a5f768b2dda2ae52a855537'
+  },
+  google_sheets: {
+    sheet_id: ''  // filled in when Google Sheets is set up
+  },
+  gemini: {
+    api_key: 'AIzaSyCQYTGQKAASUHezC2z2yGqYKKPWq0BhjTE'   // already in use for the advisor
+  }
+};
